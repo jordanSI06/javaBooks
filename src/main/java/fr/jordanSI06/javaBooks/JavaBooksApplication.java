c59c1b85f55b1628d5c85e5119f5126a67f5f0ec
Jordan <jordan.sintes@gmail.com>
2025-02-27 17:28:45 +0100

    Init commit

---
+++ src/main/java/fr/jordanSI06/javaBooks/JavaBooksApplication.java
@@ -0,0 +1,13 @@
+package fr.jordanSI06.javaBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBooksApplication.class, args);
+	}
+
+}
